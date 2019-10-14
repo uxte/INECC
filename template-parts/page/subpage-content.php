ca2fadb1df23b907f71ae05b3ff4906ad5374f0c
PEDRO REIS <pedro.reis@uxte.com>
2019-10-14 11:46:03 +0100

    Changes to sub-page

--- template-parts/page/subpage-content.php
+++ template-parts/page/subpage-content.php
@@ -6,12 +6,13 @@
 <main>
 
     <?php get_template_part( 'template-parts/page/page', 'tabs' ); ?>
-    
+
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <section class="content subpage">
         <header>
             <h1><?php the_title(); ?></h1>
+            <?php the_post_thumbnail('full', array('class' => 'icon')); ?>
         </header>
 
         <?php the_content();?>
@@ -19,27 +20,27 @@
 
     <?php endwhile; endif; ?>
 
-    <aside class="content parent">
-        <?php
-        //Get parent content
-        $parent_id = $post -> post_parent;
-
-        $args = array(
-            'page_id'                => $parent_id,
-            'post_type'              => array( 'page' ),
-            'post_status'            => array( 'publish' ),
-            'posts_per_page'         => 1
-        );
-
-        $parent_page = new WP_Query( $args );
-
-        if ( $parent_page->have_posts() ) : while ( $parent_page->have_posts() ) : $parent_page->the_post(); ?>
-
-        <h3><?php the_title(); ?></h3>
-
-        <?php the_content(); ?>
-
-        <?php endwhile; endif; wp_reset_postdata(); ?>
-    </aside>
-
 </main>
+
+<aside class="content parent">
+    <?php
+    //Get parent content
+    $parent_id = $post -> post_parent;
+
+    $args = array(
+        'page_id'                => $parent_id,
+        'post_type'              => array( 'page' ),
+        'post_status'            => array( 'publish' ),
+        'posts_per_page'         => 1
+    );
+
+    $parent_page = new WP_Query( $args );
+
+    if ( $parent_page->have_posts() ) : while ( $parent_page->have_posts() ) : $parent_page->the_post(); ?>
+
+    <h3><?php the_title(); ?></h3>
+
+    <?php the_content(); ?>
+
+    <?php endwhile; endif; wp_reset_postdata(); ?>
+</aside>
