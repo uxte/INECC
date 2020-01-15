700a9a5c3f4c352fc777745a8b7ecc08d2b748a8
PEDRO REIS <pedro.reis@uxte.com>
2020-01-15 21:57:29 +0000

    Add intro text to list posts pages

--- page-our-work-search.php
+++ page-our-work-search.php
@@ -7,16 +7,13 @@
 
     <?php get_template_part( 'template-parts/page/page', 'tabs' ); ?>
 
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
     <section class="content subpage">
-
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
         <header>
             <h1><?php the_title(); ?></h1>
             <?php the_post_thumbnail('full', array('class' => 'icon')); ?>
         </header>
-
-        <?php the_content(); ?>
 
         <?php endwhile; endif; wp_reset_postdata(); ?>
 
@@ -42,5 +39,30 @@
     </section>
 
 </main>
+
+<aside class="content parent">
+    <?php
+    //Get parent content
+    $parent_id = $post -> post_parent;
+
+    $args = array(
+        'page_id'                => $parent_id,
+        'post_type'              => array( 'page' ),
+        'post_status'            => array( 'publish' ),
+        'posts_per_page'         => 1
+    );
+
+    $parent_page = new WP_Query( $args );
+
+    if ( $parent_page->have_posts() ) : while ( $parent_page->have_posts() ) : $parent_page->the_post(); ?>
+
+    <header>
+        <h3><?php the_title(); ?></h3>
+    </header>
+
+    <?php endwhile; endif; wp_reset_postdata(); ?>
+    
+    <?php the_content(); ?>
+</aside>
 
 <?php get_footer(); ?>
