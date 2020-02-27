cd6e948f6cc09b30c436fb590d995c1b30b81617
uxte <pedro.reis@uxte.com>
2020-02-27 18:47:14 +0000

    Fix wrapper bug when no upcoming events

--- template-parts/list-upcoming-events.php
+++ template-parts/list-upcoming-events.php
@@ -26,51 +26,49 @@ if ( $posts -> have_posts() ) :
     </header>
 
     <div class="wrap grid">
-    <?php
-    // The Loop
-    while ( $posts -> have_posts() ) :
-        $posts -> the_post();
+        <?php
+        // The Loop
+        while ( $posts -> have_posts() ) :
+            $posts -> the_post();
 
-        $class_hide = '';
-        if ( $posts->current_post >= 1 && is_front_page() ) {
-            $class_hide = 'hide';
-        }
+            $class_hide = '';
+            if ( $posts->current_post >= 1 && is_front_page() ) {
+                $class_hide = 'hide';
+            }
+        ?>
 
-    ?>
-
-    <article class="event slide <?php echo $class_hide;?>" id="<?php echo $post->post_name?>">
-        <h2><a href="<?php echo esc_url( get_permalink( get_page_by_title( 'INECC Events' ) ) ) . "#" . $post->post_name ?>"><?php the_title(); ?></a></h2>
-        <div class="row">
-            <div class="col">
-                <time datetime="<?php echo get_the_date('c'); ?>" pubdate="pubdate"><?php print get_the_date('M \<\s\p\a\n\>d\<\/\s\p\a\n\>');  ?></time>
-                <div class="img"><?php print get_the_post_thumbnail( $post, 'medium' ); ?></div>
-                <!-- <figure><img src="/img/thumb.svg" alt="Thumb" /></figure> -->
+        <article class="event slide <?php echo $class_hide;?>" id="<?php echo $post->post_name?>">
+            <h2><a href="<?php echo esc_url( get_permalink( get_page_by_title( 'INECC Events' ) ) ) . "#" . $post->post_name ?>"><?php the_title(); ?></a></h2>
+            <div class="row">
+                <div class="col">
+                    <time datetime="<?php echo get_the_date('c'); ?>" pubdate="pubdate"><?php print get_the_date('M \<\s\p\a\n\>d\<\/\s\p\a\n\>');  ?></time>
+                    <div class="img"><?php print get_the_post_thumbnail( $post, 'medium' ); ?></div>
+                    <!-- <figure><img src="/img/thumb.svg" alt="Thumb" /></figure> -->
+                </div>
+                <div class="col">
+                    <address><?php print $post->event_place; ?></address>
+                    <?php the_excerpt(); ?>
+                </div>
             </div>
-            <div class="col">
-                <address><?php print $post->event_place; ?></address>
-                <?php the_excerpt(); ?>
-            </div>
-        </div>
-        <footer class="row">
-            <div class="col">
-                <a class="button icon calendar" href="<?php echo make_cal_link($post); ?>" target="_blank">Add to my <span>calendar</span></a>
-            </div>
-            <div class="col">
-                <a class="button icon facebook" rel="noreferrer nofollow" href="<?php echo $post->event_fb; ?>">Find out <span>more on</span></a>
-            </div>
-        </footer>
-    </article>
-
-    <?php endwhile; endif; wp_reset_postdata(); ?>
-
-    <?php if ( is_front_page() ) :
-        if ( $posts->post_count <= 1 ) {
+            <footer class="row">
+                <div class="col">
+                    <a class="button icon calendar" href="<?php echo make_cal_link($post); ?>" target="_blank">Add to my <span>calendar</span></a>
+                </div>
+                <div class="col">
+                    <a class="button icon facebook" rel="noreferrer nofollow" href="<?php echo $post->event_fb; ?>">Find out <span>more on</span></a>
+                </div>
+            </footer>
+        </article>
+        <?php endwhile; ?>
+        <?php
+        if ( is_front_page() && $posts->post_count <= 1 ) { // If is front page and there's 1 or less posts don't show nav
             $navClass = 'hide';
-        }?>
-    <nav class="prev-next <?php echo $navClass ?>">
-        <a href="#" rel="prev">Previous</a>
-        <a href="#" rel="next">Next</a>
-    </nav>
+        }
+        ?>
+        <nav class="prev-next <?php echo $navClass ?>">
+            <a href="#" rel="prev">Previous</a>
+            <a href="#" rel="next">Next</a>
+        </nav>
     </div>
-    <?php endif; ?>
 </section>
+<?php endif; wp_reset_postdata(); ?>
