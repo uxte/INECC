a6e44357b1ea6179f5b1e38d11f7f8d7f3851273
adonis <andrealou.adao@gmail.com>
2020-12-16 17:11:08 +0000

    upgrade sidebar php and add viewportwidth to the function

--- template-parts/sidebar.php
+++ template-parts/sidebar.php
@@ -2,53 +2,52 @@
 
 <?php if ( !is_home() && !is_page( 'Sitemap' ) ) : ?>
     <nav class="sidebar-nav" id="sidebar-nav">
-    <a class="sb-menu-toggle" id="sbMenuToggle" href="#sb-menu"><span class="open-menu">Quick menu access</span><span class="close-menu">Close menu</span></a>
-        <?php
-        wp_nav_menu( array(
-            'menu'            => 'top-menu',
-            'container'       => '',
-            'menu_id'         => 'sbMenu',
-            'menu_class'      => 'sb-menu open'
-        ));
-        ?>
+        <a class="sb-menu-toggle" id="sbMenuToggle" href="#"><span class="open-menu">Quick menu access</span><span class="close-menu">Close menu</span></a>
+            <?php
+            wp_nav_menu( array(
+                'menu'            => 'top-menu',
+                'container'       => '',
+                'menu_id'         => 'sbMenu',
+                'menu_class'      => 'sb-menu open'
+            ));
+            ?>
     </nav>
+
+    <?php
+        $pageID = get_the_ID();
+
+        $args = array(
+            'category_name'         => 'sidebar',
+            'post_type'             => 'post',
+            'post_status'           => 'publish',
+            'order'                 => 'DESC',
+            'posts_per_page'        => 1,
+            'meta_query' => array(
+            array(
+                'key'     => 'select_page',
+                'value'   => $pageID,
+                'compare' => '='
+            )
+            )
+        );
+        // The Query
+        $posts = new WP_Query( $args );
+
+        // The Loop
+        if ( $posts -> have_posts() ) : while ( $posts -> have_posts() ) : $posts -> the_post(); 
+    ?>
+
+            <div class="sb-post">
+                <?php print get_the_post_thumbnail( $post, 'medium' ); ?>
+                <div class="sb-post-content">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+
+    <?php endwhile; endif; wp_reset_postdata(); ?>
+
+        <a class="back-to-top" rel="nofollow" href="#top">Back to top</a>
+
 <?php endif; ?>
-
-<?php
-
-$pageID = get_the_ID();
-
-$args = array(
-    'category_name'         => 'sidebar',
-    'post_type'             => 'post',
-    'post_status'           => 'publish',
-    'order'                 => 'DESC',
-    'posts_per_page'        => 1,
-    'meta_query' => array(
-       array(
-          'key'     => 'select_page',
-          'value'   => $pageID,
-          'compare' => '='
-       )
-    )
-);
-// The Query
-$posts = new WP_Query( $args );
-
-// The Loop
-if ( $posts -> have_posts() ) : while ( $posts -> have_posts() ) : $posts -> the_post(); 
-?>
-<?php $select_page_value = get_post_meta( get_the_ID(), 'select_page', true ); ?>
-
-    <div class="sb-post">
-        <?php print get_the_post_thumbnail( $post, 'medium' ); ?>
-        <div class="sb-post-content">
-            <?php the_content(); ?>
-        </div>
-    </div>
-
-<?php endwhile; endif; wp_reset_postdata(); ?>
-
-<a class="back-to-top" rel="nofollow" href="#top">Back to top</a>
 
 </aside>
